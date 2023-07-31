c49603e1bfe6c592db625aa7c3ceb97dc2da5c6c
edwin <ramosaguilaredwin7@gmail.com>
2023-07-31 09:06:01 -0500

    Las listas mas que se utilizan

---
+++ main.py
@@ -0,0 +1,8 @@
+lista = []
+while len(lista) < 6:
+    mensaje = input('Ingresa cinco numeros: ')
+    lista.append(mensaje)
+
+print(f'''
+en la lista hay {lista}
+''')
---
+++ multiplicaión_co_while.py
@@ -0,0 +1,24 @@
+in_num = int(input('Escribe un número: '))
+numeros = 0
+for i in range (1,in_num):
+
+    numeros = in_num - i
+    resultado = in_num * numeros
+    nume = resultado * numeros
+
+    print(f'{in_num} * {numeros} = {nume}')
+print(resultado)
+
+print(f'La suma total es {numeros}')
+## SUCESION FIBOCCI
+## tarea mostrar la sucesion fibocci de los
+## 10 primeros numeros
+'''in_numero = int(input('Escribe un número: '))
+numeros = 0
+for i in range (1,in_numero):
+    pri_numer = numeros
+    numeros+=i
+    print(f'{pri_numer} + {i} = {numeros}')
+
+print(f'La suma total es {numeros}')'''
+    
